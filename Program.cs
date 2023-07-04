058977c7b14c892ebf5ca1b715e63af90f274c6f
osamaline1229 <osama98138139@gmail.com>
2023-07-03 23:54:39 -0700

    basic Exception

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Exception;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        student[] students =
+        {
+            new student("salim", "16338", 79),
+            new student("Ahmed", "12230", 26),
+            new student("osama", "12230", 30)
+        };
+        Console.WriteLine(student.VlodateStudents(students));
+    }
+}
---
+++ student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception
+{
+    internal class student
+    {
+        string name { get; set; }
+        string registrationNumber { get; set; }
+
+        int age { get; set; }
+
+        public student(string name, string registrationNumber, int age)
+        {
+            this.name = name;
+            this.registrationNumber = registrationNumber;
+            this.age = age;
+        }
+        public static Boolean VlodateStudents(student[] students) 
+        {
+            for (int i = 0; i < students.Length; i++)
+            {
+                for(int j = 0; j < students.Length; j++)
+                {
+
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
